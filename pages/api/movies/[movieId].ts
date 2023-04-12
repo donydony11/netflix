7d0ad6e0203dcb5a9adcacc6548c0fa2f17f277f
donydony11 <121674533+donydony11@users.noreply.github.com>
2023-04-12 18:50:56 +0300

    C1

--- pages/api/movies/[movieId].ts
+++ pages/api/movies/[movieId].ts
@@ -1,36 +1,34 @@
-import  prismadb from '@/lib/prismadb';
-import { NextApiRequest, NextApiResponse } from "next"
+import prisma from '@/lib/prismadb';
+import { NextApiRequest, NextApiResponse } from 'next';
 import serverAuth from '@/lib/serverAuth';
 
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'GET') {
+    return res.status(405).end();
+  }
 
-export default async function(req: NextApiRequest, res: NextApiResponse){
-    if(req.method != "GET"){
-        return res.status(405).end()
-    }
-    try{
-        await serverAuth(req, res)
+  try {
+    await serverAuth(req, res);
 
-        const { movieId } = req.query
+    const { movieId } = req.query;
 
-        if(typeof movieId != 'string'){
-            throw new Error('Invalid ID')
-        }
-        if(!movieId){
-            throw new Error('Invalid ID')
-        }
-    
-    const movie = await prismadb.movie.findUnique({
-        where: {
-            id: movieId
-        }
-    })
-    if(!movie){
-        throw new Error('Invalid ID')
+    if (typeof movieId !== 'string' || !movieId) {
+      return res.status(400).json({ message: 'Invalid ID' });
     }
-    return res.status(200).json(movie)
-}
-    catch(error){
-console.log(error)
-        return res.status(400).end()
+
+    const movie = await prisma.movie.findUnique({
+      where: {
+        id: movieId,
+      },
+    });
+
+    if (!movie) {
+      return res.status(404).json({ message: 'Movie not found' });
     }
+
+    return res.status(200).json(movie);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
 }
