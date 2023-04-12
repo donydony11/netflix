da19cf611a976b16b7702088e61ab0e709413411
donydony11 <121674533+donydony11@users.noreply.github.com>
2023-04-12 18:26:15 +0300

    1

--- pages/watch/[movieId].tsx
+++ pages/watch/[movieId].tsx
@@ -13,7 +13,9 @@ const Watch = () => {
       <nav className="fixed w-full p-4 z-10 flex  flex-row items-center gap-8 bg-black bg-opacity-70">
         <AiOutlineArrowLeft
           onClick={() => router.push("/")}
-          className="text-white size={40} cursor-pointer"
+          className="text-white"
+          size={40}
+          cursor="pointer"
         />
         <p className="text-white text-1xl lg:text-3xl font-bold">
           <span className="font-light mr-2">Watching:</span>
